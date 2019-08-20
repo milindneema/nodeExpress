778fc4d7cc6a5ec0663940a39e93ed291df997a4
abc <abc@example.com>
2019-08-20 15:20:41 +0530

    delete is included in inbox and send message query written

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const con = require('./model/db');
 const mysql = require('mysql');
+const session = require('express-session');
 //creating expresss ap
 const app =express();
 //create and start server  
@@ -10,6 +11,7 @@ app.listen(3000,()=>{
 });
 
 //to serve static files
+app.use(session({secret:"1234567"}));
 app.use(express.static('public'));
 
 
@@ -17,6 +19,12 @@ app.use(express.static('public'));
 var  path = require('path');                    //solution for doutes of servers
 app.set('views',path.join(__dirname,'views'));  //setting location
 app.set('view engine','hbs');                   //setting extention
+
+// caching disabled for every route
+app.use(function(req, res, next) {
+res.set('Cache-Control', 'no-cache, private, no-store,must-revalidate,max-stale=0, post-check=0, pre-check=0');
+    next();
+});
 
 //first page has no name
 //get method will run the app
@@ -43,16 +51,47 @@ app.get('/',(request,responce)=>{
      responce.render('registration');
  });
 
+ app.get('/send',(request,responce)=>{
+    responce.render('send');
+});
+
  app.get('/home',(request,responce)=>{
     responce.render('home');
 });
 
-app.get('/send',(request,responce)=>{
-    responce.render('send',{user:userid});
+
+app.post('/sendreq',(request,responce)=>{
+    var rec=request.body.remail;
+    var sub=request.body.subject;
+    var msg=request.body.message;
+    sql = "insert into mail(sender,receiver,subject,message) values(?,?,?,?)"
+    var input = [request.session.userid,rec,sub,msg];
+    sql = mysql.format(sql,input);
+    con.query(sql,(err)=>{
+        if (err) throw err;
+        else{
+            request.render('send',{msg:"Message Sent"});
+        }
+    });
+    
 });
 
 app.get('/delete',(request,responce)=>{
-    responce.render('delete',{user:userid});
+    var mid = request.query.mid;
+    var sql = "delete from mail where mid ="+mid;
+    con.query(sql,(err)=>{
+        if(err) throw err;
+        else{
+          var sql="select * from employee";
+          con.query(sql,(err,result)=>{
+            if(err) throw err;
+            else
+            response.render('inbox',{data:result,msg:"Data Deleted"}); //1) extention 2) location
+        });
+    }
+
+        });
+      
 });
 
 app.get('/change',(request,responce)=>{
@@ -68,8 +107,9 @@ app.post('/logincheck',(request,responce)=>{
     con.query(sql,(err,result)=>{
         if (err) throw err;
         else if(result.length>0){
+            request.session.userid=userid;        
              //responce.render('index',{msg:'login success'});
-        responce.render('home',{user:userid});
+        responce.render('home',{user:request.session.userid});
     } else
    responce.render('index',{msg:'login fail'});
         
@@ -84,7 +124,7 @@ app.post('/register',(request,responce)=>{
     var sql = "insert into account(name,emailid,password) values(?,?,?);";
     var input = [name,userid,pass];
     sql = mysql.format(sql,input);
-    pool.query(sql, function (err1) {
+    con.query(sql, function (err1) {
         if (err1) throw err1;
         else{
             responce.render('index',{msg:'Registration successful'});
@@ -92,6 +132,25 @@ app.post('/register',(request,responce)=>{
     });
 
 });
+
+app.get('/inbox',(request,responce)=>{
+    var sql = "select * from mail where receiver = ?;";
+    var input = [userid];
+    sql = mysql.format(sql,input);
+    con.query(sql, function (err1,result) {
+        if (err1) throw err1;
+        else{
+            console.log(result);
+            
+        responce.render('inbox',{data:result});
+        }
+});
+});
+
+app.get('/logout',(request,response)=>{
+    request.session.destroy();
+    response.render('index');
+    });
 
 app.use(function(req,res){
     res.status(404);
